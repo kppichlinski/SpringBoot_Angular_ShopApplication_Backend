4f9a25aeb14c96513a5d1039f28461fef24b2a2f
kpichlinski <kppichlinski@gmail.com>
2023-01-30 16:50:32 +0100

    products paging

--- src/main/java/com/example/shop/product/service/ProductService.java
+++ src/main/java/com/example/shop/product/service/ProductService.java
@@ -3,9 +3,10 @@ package com.example.shop.product.service;
 import com.example.shop.product.model.Product;
 import com.example.shop.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -13,7 +14,7 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
-    public List<Product> getProducts() {
-        return productRepository.findAll();
+    public Page<Product> getProducts(Pageable pageable) {
+        return productRepository.findAll(pageable);
     }
 }
