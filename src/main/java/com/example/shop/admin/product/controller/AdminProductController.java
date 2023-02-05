0f61132a354506a34c3ae2fb2301a3b6ec04bc2c
kpichlinski <kppichlinski@gmail.com>
2023-02-05 18:53:48 +0100

    adding and updating products as admin

--- src/main/java/com/example/shop/admin/product/controller/AdminProductController.java
+++ src/main/java/com/example/shop/admin/product/controller/AdminProductController.java
@@ -1,21 +1,51 @@
 package com.example.shop.admin.product.controller;
 
+import com.example.shop.admin.product.dto.AdminProductDto;
 import com.example.shop.admin.product.model.AdminProduct;
 import com.example.shop.admin.product.service.AdminProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
 @RequiredArgsConstructor
 public class AdminProductController {
+
+    public static final Long EMPTY_ID = null;
 
     private final AdminProductService adminProductService;
 
     @GetMapping("admin/products")
     public Page<AdminProduct> getProducts(Pageable pageable) {
         return adminProductService.getProducts(pageable);
+    }
+
+    @GetMapping("admin/products/{id}")
+    public AdminProduct getProduct(@PathVariable Long id) {
+        return adminProductService.getProduct(id);
+    }
+
+    @PostMapping("admin/products")
+    public AdminProduct createProduct(@RequestBody AdminProductDto adminProductDto) {
+        return adminProductService.createProduct(mapToAdminProduct(adminProductDto, EMPTY_ID));
+    }
+
+    @PutMapping("admin/products/{id}")
+    public AdminProduct updateProduct(@RequestBody AdminProductDto adminProductDto, @PathVariable Long id) {
+        return adminProductService.updateProduct(mapToAdminProduct(adminProductDto, id));
+    }
+
+    private AdminProduct mapToAdminProduct(AdminProductDto adminProductDto, Long id){
+        return AdminProduct.builder()
+                .id(id)
+                .name(adminProductDto.getName())
+                .description(adminProductDto.getDescription())
+                .category(adminProductDto.getCategory())
+                .price(adminProductDto.getPrice())
+                .currency(adminProductDto.getCurrency().toUpperCase(Locale.ROOT))
+                .build();
     }
 }
--- src/main/java/com/example/shop/admin/product/model/AdminProduct.java
+++ src/main/java/com/example/shop/admin/product/model/AdminProduct.java
@@ -2,6 +2,7 @@ package com.example.shop.admin.product.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -12,6 +13,7 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Builder
 public class AdminProduct {
 
     @Id
