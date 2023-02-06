13fcd7dfd470639ff1a5311c7bf6891e79c70121
kpichlinski <kppichlinski@gmail.com>
2023-02-06 22:55:13 +0100

    default language set to US

---
+++ src/main/java/com/example/shop/config/Config.java
@@ -0,0 +1,19 @@
+package com.example.shop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+@Configuration
+public class Config {
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.US);
+        return slr;
+    }
+}
