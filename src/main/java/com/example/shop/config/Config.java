cfd48a54e5706d18e74f44d3df41ebed8074677a
kpichlinski <kppichlinski@gmail.com>
2023-11-27 21:09:39 +0100

    passing jwt to swagger

--- src/main/java/com/example/shop/config/Config.java
+++ src/main/java/com/example/shop/config/Config.java
@@ -1,10 +1,16 @@
 package com.example.shop.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
+import java.util.Arrays;
 import java.util.Locale;
 
 @Configuration
@@ -15,5 +21,30 @@ public class Config {
         SessionLocaleResolver slr = new SessionLocaleResolver();
         slr.setDefaultLocale(Locale.US);
         return slr;
+    }
+
+    @Bean
+    public OpenAPI api() {
+        return new OpenAPI()
+                .components(
+                        new Components().addSecuritySchemes(
+                                "JWT Token",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .in(SecurityScheme.In.HEADER)
+                                        .name("Authorization")
+                        )
+                )
+                .info(
+                        new Info()
+                        .title("Shop API")
+                        .version("0")
+                )
+                .addSecurityItem(
+                        new SecurityRequirement()
+                                .addList("JWT Token", Arrays.asList("read", "write"))
+                );
     }
 }
